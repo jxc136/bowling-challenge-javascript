2a1604a7c10c2e420c0585f918b89a5edc152af8
Josh Carvell <j_carvell@hotmail.co.uk>
2023-01-01 18:16:37 +0000

    added error handling to ensure frames exceeding 10 pins cannot be added, added pass ing tests for error handling and the "isSpare function"

--- test/frame.test.js
+++ test/frame.test.js
@@ -1,11 +1,28 @@
 const Frame = require('../lib/frame');
 
 describe ('Frame', () => {
+
   it ('returns a score between 1 and 10', () => {
     const frame = new Frame(1, 5);
-    expect(frame.isStrike()).toEqual (false)
-    expect(frame.isSpare()).toEqual (false)
-    expect(frame.frameTotal()).toEqual (6)
+    expect(frame.isStrike()).toEqual(false)
+    expect(frame.isSpare()).toEqual(false)
+    expect(frame.frameTotal()).toEqual(6)
   });
+
+  it ('raises and error if the sum of the frame arguments exceeds 10', () => {
+    const frame = new Frame(10, 5);
+    try {
+      frame.frameTotal();
+    } catch (error) {
+      expect(error.message).toBe('invalid score');
+    }});
+
+  it ('identifies a frame with a spare', () => {
+    const frame = new Frame(6, 4);
+    expect(frame.isStrike()).toEqual (false)
+    expect(frame.isSpare()).toEqual (true)
+    expect(frame.frameTotal()).toEqual (10)
+  });
+  
    
 });
