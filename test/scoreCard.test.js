a4625e7c544daa008310f5c7f67057f8a5d159e4
Josh Carvell <j_carvell@hotmail.co.uk>
2023-01-02 02:16:28 +0000

    added resolveSpare method

--- test/scoreCard.test.js
+++ test/scoreCard.test.js
@@ -10,6 +10,22 @@ describe ('ScoreCard', () => {
     const testScorecard =  { rollOne: 5, rollTwo: 3, frameTotal: 8, isStrike: false, isSpare: false, bonusStatus:false }
     expect(scorecard.displayScore()).toContainEqual(testScorecard);
   })
+
+  it ('Correctly scores a spare into an open frame', () => {
+    const scoreCard = new Scorecard();
+    const frame1 = new Frame(6, 4);
+    const frame2 = new Frame(5, 2);
+    scoreCard.add(frame1);
+    scoreCard.add(frame2);
+    expect(frame1.bonusStatus()).toEqual(true);
+    // scorecard.calculateBonuses();
+    scoreCard.resolveSpare();
+    const expectedFrame1 = { rollOne: 6, rollTwo: 4, frameTotal: 15, isStrike: false, isSpare: true, bonusStatus: false };
+    const expectedFrame2 = { rollOne: 5, rollTwo: 2, frameTotal: 7, isStrike: false, isSpare: false, bonusStatus: false };
+    expect(scoreCard.displayScore()).toContainEqual(expectedFrame1);
+    expect(scoreCard.displayScore()).toContainEqual(expectedFrame2);
+   
+  })
 })
 
 
